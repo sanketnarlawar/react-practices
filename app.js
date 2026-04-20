e813c679d0d9ec718e3d9563a26292b70701639e
sanketnarlawar <sanketnarlawar@gmail.com>
2026-04-20 15:38:19 +0530

    feat: update React setup and enhance app structure with new components

--- app.js
+++ app.js
@@ -10,24 +10,25 @@
     </div>
 </div> */}
 
+import React from "react";
 // reactElement(object) => HTML(Browser Undersands)
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-   [ React.createElement(
-        "div",
-        {id:"children1"},
-       [  React.createElement("h1",{}, "i am an h1 tag"), React.createElement("h2",{}, "i am an h2 tag")
-   ] ), React.createElement(
-        "div",
-        {id:"children2"},
-       [  React.createElement("h1",{}, "i am an h1 tag"), React.createElement("h2",{}, "i am an h2 tag")
-   ] )]
-)
+import react from "react"; 
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div",{id:"parent"},[
+         React.createElement("div",{id:"children1"},[ 
+             React.createElement("h1",{}, "i am Sanket Narlawar 🚀"),
+             React.createElement("h2",{}, "Hi all of you ")
+   ] ), 
+   React.createElement("div",{id:"children2"},[
+        React.createElement("h1",{}, "i am an h1 tag"),
+        React.createElement("h2",{}, "i am an h2 tag")
+   ] ),
+]);
 // const heading = React.createElement("h1",{id : "heading",className:"heading"},"hello world to the react!");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
  root.render(parent);
 
- console.log(parent.props.children);
+ console.log(parent);
