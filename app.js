a73f4125c5615ddb8868169c422250b3a35b48c8
sanketnarlawar <sanketnarlawar@gmail.com>
2026-06-02 14:40:52 +0530

    function based component , jsx element rendering .

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ import reactDOM from "react-dom/client";
 // react.createElement => object => HTML Element render
 
 //React/JSXS Element
-const element =<h1>This Is Element By JSX </h1>
+const element =(
+ <h1 className="head" tabIndex="5">
+    This Is Element By JSX \
+ </h1>
+);
 
 //React Component
 const JsxHeading = () => (
@@ -17,6 +21,8 @@ const JsxHeading = () => (
 // React Component \
 
 // Class Based Component - old way of writing component
+
+// Function Based Component- new way of writing component
 const HeadingComponent = ()=> (
     <div id="container">
     {element}
@@ -24,7 +30,6 @@ const HeadingComponent = ()=> (
         <h1 id="heading"> This Is The Heading By Function Based Component </h1>
     </div>
 );
-// Function Based Component- new way of writing component
 
 
 const root = reactDOM.createRoot(document.getElementById("root"));
